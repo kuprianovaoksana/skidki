597fa15243d105664a131e9c6c503fa57fb3e05a
LEONID JOFE <121107493+StudentFPW@users.noreply.github.com>
2023-09-15 15:10:28 +0300

    Delete backend directory

--- backend/djangoscrapy/djangoscrapy/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoscrapyConfig(AppConfig):
-    """
-    Класс DjangoscrapyConfig — это класс конфигурации приложения djangoscrapy.
-    """
-    name = 'djangoscrapy'
