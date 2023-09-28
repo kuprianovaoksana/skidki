ef6c7a525e4861934009f9821ef51719d51bafb2
LEONID JOFE <121107493+StudentFPW@users.noreply.github.com>
2023-09-28 13:46:05 +0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +69,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True  # FIXME THIS IS ON TEST MODE
+
+# CORS_ORIGIN_WHITELIST = (
+#     'google.com',
+#     'hostname.example.com',
+#     'localhost:8000',
+#     '127.0.0.1:9000'
+# )
 
 ROOT_URLCONF = 'config.urls'
 
