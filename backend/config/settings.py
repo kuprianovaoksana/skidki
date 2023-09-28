1874a1e271c92fc9f021c044236cd0228617cd79
LEONID JOFE <121107493+StudentFPW@users.noreply.github.com>
2023-09-28 13:42:57 +0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,8 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # FIXME FOR SERVER
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
